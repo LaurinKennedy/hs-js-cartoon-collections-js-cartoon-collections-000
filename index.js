7c4bdee189b1d1cb199a0115017ffc5200620a18
Laurin Kennedy <laurinmk@gmail.com>
2020-05-29 00:35:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ function dwarfRollCall(dwarves) {
   //Loop through an array of dwarves to 
   //return string of numbered dwarves "1. Doc 2.Dopey 3.Bashful"
   var numberedDwarves = []
-    // set new variable to numberedDwarves = empty array to have a place to push a new value of index(+1 because index starts at 0 and I need it to start at 1). and the value of the dwarf at the corresponding index 
+    // set new variable to numberedDwarves = empty array to have a place to push a new value of index (+1 because index starts at 0 and I need it to start at 1). and the value of the dwarf at the corresponding index into 
   for (let i = 0 ; i < dwarves.length ; i++) {
+    //use a for loop to loop thought all the dwarves in the array
     numberedDwarves.push(`${i+1}. ${dwarves[i]} `)
+    //pushed this value into the new array 
   }
   return numberedDwarves.join("")
 }
